d9942b5ddb6c20def9c8aff14d4c46f2acab70be
Pablo Ezequiel Romero <romero7116@gmail.com>
2022-02-03 10:50:51 +0100

    Revert "Removed SwiftFormat"
    This reverts commit 157c8ea7f735dce217ff99ab1acabffdc0a9504f.

--- GraphQL-CodeGenerator/Generator.swift
+++ GraphQL-CodeGenerator/Generator.swift
@@ -79,9 +79,11 @@ public struct Generator {
         let schema = try loadSchema(from: schemaPath)
         let context = buildContext(from: schema)
 
-        let rendered = try environment.renderTemplate(
+        var rendered = try environment.renderTemplate(
             name: templateName,
             context: context)
+
+//        rendered = try rendered.format()
 
         print(rendered)
 
